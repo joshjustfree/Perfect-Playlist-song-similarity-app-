5483205014043a3457c54c69aa44a429ab1ebf78
Joshua Akaonye <joshuaakaonye@gmail.com>
2026-03-08 20:43:55 +0200

    Updated the file by changing function names and their indentations, due to the errors it was causing in app.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,5 @@
 # The SQLite database functions
-
 import sqlite3
-from unittest import result
 
 DATABASE_PATH = '../database/search_history.db'
 
@@ -22,7 +20,7 @@ def init_db():
 
     # Favorite Songs table
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS favorite_songs (
+        CREATE TABLE IF NOT EXISTS favourite_songs (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             song_title TEXT,
             artist_name TEXT,
@@ -34,57 +32,57 @@ def init_db():
     conn.close()
     print("Database initialized successfully.")
 
-    # Saving search history to the database
-    def save_search(song_title, artist_name):
-        conn = sqlite3.connect(DATABASE_PATH)
-        cursor = conn.cursor()
-        cursor.execute(' INSERT INTO search_history (song_title, artist_name) VALUES (?, ?)', (song_title, artist_name))
+# Saving search history to the database
+def save_search(song_title, artist_name):
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO search_history (song_title, artist_name) VALUES (?, ?)', (song_title, artist_name))
 
-        conn.commit()
-        conn.close()
+    conn.commit()
+    conn.close()
 
-    # Gettiing all searches 
-    def get_search_history():
-        conn = sqlite3.connect(DATABASE_PATH)
-        cursor = conn.cursor()
-        cursor.execute('SELECT song_title, artist_name, timestamp FROM search_history ORDER BY timestamp DESC')
-        results = cursor.fetchall()
-        conn.close()
-        
-        history = []
-        for row in results:
-            history.append({
-                'song': row[0],
-                'artist': row[1],
-                'time': row[2]
-            })
-        return history
+# Gettiing all searches 
+def get_search_history():
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute('SELECT song_title, artist_name, timestamp FROM search_history ORDER BY timestamp DESC')
+    results = cursor.fetchall()
+    conn.close()
     
-    # Saving favorite songs to the database
-    def add_favorite(song_title, artist_name):
-        conn = sqlite3.connect(DATABASE_PATH)
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO favorites (song_title, artist_name) VALUES (?, ?)', (song_title, artist_name))
+    history = []
+    for row in results:
+        history.append({
+            'song': row[0],
+            'artist': row[1],
+            'time': row[2]
+        })
+    return history
+    
+# Saving favourite songs to the database
+def add_favourite(song_title, artist_name):
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO favourite_songs (song_title, artist_name) VALUES (?, ?)', (song_title, artist_name))
 
-        conn.commit()
-        conn.close()
+    conn.commit()
+    conn.close()
 
-    # Getting all favorite songs
-    def get_favorites():
-        conn = sqlite3.connect(DATABASE_PATH)
-        cursor = conn.cursor()
-        cursor.execute('SELECT song_title, artist_name, FROM favorites ORDER BY timestamp DESC')
-        results = cursor.fetchall()
-        conn.close()
-        
-        favorites = []
-        for row in results:
-            favorites.append({
-                'song': row[0],
-                'artist': row[1]
-            })
-        return favorites
+# Getting all favourite songs
+def get_favourites():
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute('SELECT song_title, artist_name FROM favourite_songs ORDER BY timestamp DESC')
+    results = cursor.fetchall()
+    conn.close()
+    
+    favourites = []
+    for row in results:
+        favourites.append({
+            'song': row[0],
+            'artist': row[1]
+        })
+    return favourites
     
 
-    if __name__ == '__main__':
-        init_db()
+if __name__ == '__main__':
+    init_db()
