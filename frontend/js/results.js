f93c9261b2da9c4c46dd1c31b6966d4555dce3d0
Joshua Akaonye <joshuaakaonye@gmail.com>
2026-03-22 19:46:31 +0200

    The JavaScript responsible for displaying the results of the search

--- frontend/js/results.js
+++ frontend/js/results.js
@@ -1,1 +1,31 @@
-// Results display and management for the frontend
+// Displaying the results of the similarity search
+
+// Get the search results
+const results = JSON.parse(sessionStorage.getItem('searchResult'));
+
+
+
+// If there are no results, just go back to the search page
+if (!results) {
+    window.location.href = 'index.html';
+}
+
+
+// Get the similar songs
+const similarSongs = results.similar_songs;
+const songsList = document.getElementById('songsList');
+
+
+// Display the similar songs
+for (let i = 0; i < similarSongs.length; i++) {
+    const song = similarSongs[i];
+
+    // Create a card for each of the songs
+    songsList.innerHTML += `
+        <div class="song-card">
+            <h3>${song.name}</h3>
+            <p>${song.artists}</p>
+        </div>
+    `;
+}
+
