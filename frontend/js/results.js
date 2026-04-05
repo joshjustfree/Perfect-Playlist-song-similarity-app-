66f48259f95b99ce929d3839f053b961ca3b2161
Joshua Akaonye <joshuaakaonye@gmail.com>
2026-04-05 18:19:03 +0200

    Added a function used to add a song to the favourites list

--- frontend/js/results.js
+++ frontend/js/results.js
@@ -2,6 +2,7 @@
 
 // Get the search results
 const results = JSON.parse(sessionStorage.getItem('searchResult'));
+const API_URL = 'http://127.0.0.1:5000';
 
 // If there are no results, just go back to the search page
 if (!results) {
@@ -23,6 +24,32 @@ for (let i = 0; i < similarSongs.length; i++) {
         <div class="song-card">
             <h3>${song.name}</h3>
             <p>${song.artists}</p>
+            <button class="add-fav-btn" onclick="addToFavourites('${song.name.replace(/'/g, "\\'")}', '${song.artists.replace(/'/g, "\\'")}', event)">Add to Favourites</button>
         </div>
     `;
+}
+
+// The function to add a song to the favourites list when the user clicks a button
+function addToFavourites(songName, artistName, event) {
+    const button = event.target;
+
+    // Send it to the backend to add it to the favourites
+    fetch(`${API_URL}/api/favourites`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: songName, artist: artistName })
+    })
+    .then(response => response.json())
+    .then(data => {
+        
+        // Change the button to indicate that the song has been added to favourites
+        button.textContent = 'Song Added!';
+        button.disabled = true;
+        button.classList.add('added');
+    })
+    .catch(error => {
+        console.error('Error adding to favourites:', error);
+    });
 }
